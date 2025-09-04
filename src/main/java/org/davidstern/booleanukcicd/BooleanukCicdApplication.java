5c7df012501025658801b66b554656e621359378
David Stern <davidestern@icloud.com>
2025-09-04 10:20:49 +0200

    init

---
+++ src/main/java/org/davidstern/booleanukcicd/BooleanukCicdApplication.java
@@ -0,0 +1,13 @@
+package org.davidstern.booleanukcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooleanukCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooleanukCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/davidstern/booleanukcicd/BooleanukCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.davidstern.booleanukcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooleanukCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
